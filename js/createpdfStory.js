d5fced8b0c741ff47ccbe6c48dbf90191420c5eb
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-11-24 16:58:29 +0100

    test 123

--- js/createpdfStory.js
+++ js/createpdfStory.js
@@ -71,7 +71,7 @@ $("#controls").submit(function() {
 
       document.write("<br />");
 
-      document.write('<p> <input type="text" placeholder="Add requirement by hand to PDF" id="addRequirementValue" length="300"></input> <input type="submit" value="Add requirement" id="addRequirement"></input></p>')
+      document.write('<p> <input type="text" placeholder="Add requirement by hand to PDF" id="addRequirementValue"></input> <input type="submit" value="Add requirement" id="addRequirement"></input></p>')
       
 
       var requirementsArraz = [];
@@ -112,6 +112,7 @@ $("#controls").submit(function() {
         $('#btnOpenPDF').css('background-color', '#26BF59').css('border', '5px solid #26BF59').css('color', '#FFF');
         $('#btnSavePDF').css('background-color', '#26BF59').css('border', '5px solid #26BF59').css('color', '#FFF');
         $('#btnNewStory').css('background-color', '#FFD200').css('border', '5px solid #FFD200');
+        $('#addRequirementValue').css('background-color', '#E0E0E0').css('border', '0px solid #FFD200').css('border-bottom', '1px solid #616161').css('font-family', 'Roboto').css('width', '327px').css('color', '#FFF');
       }
 
        
