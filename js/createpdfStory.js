0219169abda8f6aa9ae6cfe81e29aab1c28abbe3
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-11-25 14:14:14 +0100

    ssss

--- js/createpdfStory.js
+++ js/createpdfStory.js
@@ -176,7 +176,7 @@ $("#controls").submit(function() {
                             body: [
                                 [{ text: [ 'TICKET-ID: \n \n', { text: storyDataNumber, style: 'mediumText', alignment: 'center' }, '\n \n'], colSpan: 3 }, '', '', 
                                  { text: [ 'TITLE: \n \n', { text: storyDataName, style: 'bigTextBold', alignment: 'center'  }, '\n \n'], colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '', 
-                                 { text: 'PRIORITY:\n' + '\n \n', colSpan: 3, rowSpan: 2 }, ''],
+                                 { text: 'PRIORITY:\n' + '\n \n', colSpan: 3, rowSpan: 2 }, '', ''],
 
                                 [{ text: [ 'EPIC:\n', { text: storyDataEpic, style: 'mediumText', alignment: 'center' }], colSpan: 11 }, '', '', '', '', '', '', '', '', '', '', 
                                  { text: ['GROOM: \n', { text: storyDataGroom, style: 'bigText', alignment: 'center' }, '\n'], colSpan: 2, }, '', 
