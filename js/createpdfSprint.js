303b9c668458e0b053d14fd4dfeb0e76e171e391
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-11-30 15:45:15 +0100

    sdfdfss

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -145,7 +145,7 @@ $("#controlsSprint").submit(function() {
       }
       
       // init the looping
-      fetchSprintDataStoryRequirements(0);
+      fetchSprintDataStoryRequirements(1);
 
     
             
