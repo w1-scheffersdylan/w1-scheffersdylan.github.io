7ce963a766d61dd734550ca78cdda0f2f49079c0
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-11-30 11:48:22 +0100

    dfsdfs

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,7 @@ var ahapdfApp = angular.module('ahapdfApp',['ngMaterial']);
 
 ahapdfApp.controller('homeController', function ($scope)
 {
-	$scope.userState = '';
+	$scope.userState = 'Hello';
+
     $scope.states = ('WEB INSPECT').split(' ').map(function (state) { return { abbrev: state }; });
 }); // end controller     
