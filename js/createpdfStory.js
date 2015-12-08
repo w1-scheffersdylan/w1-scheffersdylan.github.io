2e8af34f9f0fb045b033f0c1c5220682b662098d
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-08 12:03:32 +0100

    sdf

--- js/createpdfStory.js
+++ js/createpdfStory.js
@@ -79,7 +79,7 @@ $("#controls").submit(function() {
         api.get("/features/" + storyNumber + "/comments", {}, function(response) {
 
           //var storyDataComment = response.comments["body"];
-          console.log(response);
+          alert(response);
           // console.log(response[0].body);
           console.log(response.body);
           console.log(response['body']);
