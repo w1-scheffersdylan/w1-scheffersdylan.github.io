cf4ea69035e76e0f8a4d8acf33ae0543c18d3f03
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-08 16:42:01 +0100

    sdfa

--- lt/js/createpdfStory.js
+++ lt/js/createpdfStory.js
@@ -1,12 +1,12 @@
 $("#controls").submit(function() {
 
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://leantesting.com/login/oauth/authorize", false);
+  xhr.open("GET", "https://leantesting.com/login/oauth/authorize/Jvu68zct2aG1Xx4bEefGibq33izoUdsenIW6fO29", false);
   xhr.send();
 
   console.log(xhr.status);
   console.log(xhr.statusText);
-  
+
   return false;
   
 });
