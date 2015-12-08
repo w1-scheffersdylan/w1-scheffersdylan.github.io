dd56a48a09ed4877f70d5bc8932f3e6bcd0d2b5f
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-08 11:11:04 +0100

    adfs

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -121,7 +121,7 @@ $("#controlsSprint").submit(function() {
           }
 
           // check if their is a grooming point
-          if( JSON.stringify(response.feature.original_estimate) == 'null' || JSON.stringify(response.feature.original_estimate) == '' || JSON.stringify(response.feature.original_estimate) == 'undefined'){
+          if( typeof JSON.stringify(response.feature.original_estimate) === 'null' || typeof JSON.stringify(response.feature.original_estimate) === '' || typeof JSON.stringify(response.feature.original_estimate) === 'undefined'){
             SprintDataStoryGroom.push("");
           }
           else {
