f5250018b1644deea441e680992ca7c5c014cb92
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-17 15:14:37 +0100

    dasf

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -65,7 +65,7 @@ $("#controlsSprint").submit(function() {
           SprintDataStoryDescription.push(response.feature.description.body.replace(/(<([^>]+)>)/ig,"").replace(/&nbsp;/gi,' ').replace(/&amp;/gi,' '));
           SprintDataStoryTitle.push(response.feature.name);
           storyPosition.push(response.feature.positions);
-          
+          console.log(storyPosition);
           //console.log(response.feature);
 
 
@@ -95,7 +95,7 @@ $("#controlsSprint").submit(function() {
 
       // init the looping
       fectchSprintDataStoryGroomDescription(0);
-      console.log(storyPosition);
+      
 
       // add story numbers to requirement and notes array
       var SprintDataStoryRequirements = [];
