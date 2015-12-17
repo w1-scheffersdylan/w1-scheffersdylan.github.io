3d9b23dcd1923da19613479b6a4213b42b5e5045
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-17 15:15:26 +0100

    dfasd

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -64,7 +64,7 @@ $("#controlsSprint").submit(function() {
 
           SprintDataStoryDescription.push(response.feature.description.body.replace(/(<([^>]+)>)/ig,"").replace(/&nbsp;/gi,' ').replace(/&amp;/gi,' '));
           SprintDataStoryTitle.push(response.feature.name);
-          storyPosition.push(response.feature.positions);
+          storyPosition.push(JSON.stringify(response.feature.positions));
           console.log(storyPosition);
           //console.log(response.feature);
 
