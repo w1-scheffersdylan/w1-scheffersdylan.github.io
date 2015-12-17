b1d93e6271c77d3b490f8a5946b2f95c5b0e81c3
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-17 16:23:16 +0100

    xczxcz

--- js/controller.js
+++ js/controller.js
@@ -4,4 +4,12 @@ var ahapdfApp = angular.module('ahapdfApp',['ngMaterial']);
 ahapdfApp.controller('homeController', function ($scope)
 {
 
+
+	var self = this, j= 0, counter = 0;
+    self.mode = 'query';
+    self.activated = true;
+    self.determinateValue = 30;
+    self.modes = [ ];
+
+
 }); // end controller     
