b538e4dfc80392bcf80378737494469a9107e18b
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-17 17:27:54 +0100

    sdf

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -177,7 +177,7 @@ $("#controlsSprint").submit(function() {
              } // end for
         
           
-          }, 500 * SprintDataStoryName);
+          }, 500 * SprintDataStoryName.length);
 
         
 
@@ -220,16 +220,16 @@ $("#controlsSprint").submit(function() {
 
 
 
-// var seconds = (SprintDataStoryName.length / 2) - 1;
-//       var counter = seconds.toFixed(0);
-//       var interval = setInterval(function() {
-//           counter--;
-//           $("#requirementsDetails").text("Please wait " + counter + " seconds");
-//           if (counter == 0) {
-//               // Display a login box
-//               clearInterval(interval);
-//           }
-//       }, 1000);
+var seconds = (SprintDataStoryName.length / 2) - 1;
+      var counter = seconds.toFixed(0);
+      var interval = setInterval(function() {
+          counter--;
+          $("#requirementsDetails").text("Please wait " + counter + " seconds");
+          if (counter == 0) {
+              // Display a login box
+              clearInterval(interval);
+          }
+      }, 1000);
 
 
 
