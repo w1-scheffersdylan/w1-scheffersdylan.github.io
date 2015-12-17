fdd1d3155c74864a135fea28904ff305efd1f6da
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-17 16:56:24 +0100

    adf

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -225,14 +225,30 @@ $("#controlsSprint").submit(function() {
 
 
         
-var self = this, j= 0, counter = 0;
-self.mode = 'query';
-self.activated = true;
-self.determinateValue = 0.5 * SprintDataStoryName.length;
-            
+    var self = this, j= 0, counter = 0;
+    self.mode = 'query';
+    self.activated = true;
+    self.determinateValue = 0.5 * SprintDataStoryName.length;
+     self.modes = [ ];       
 
               
-
+      $interval(function() {
+      self.determinateValue += 1;
+      self.determinateValue2 += 1.5;
+      if (self.determinateValue > 100) self.determinateValue = 30;
+      if (self.determinateValue2 > 100) self.determinateValue2 = 30;
+        // Incrementally start animation the five (5) Indeterminate,
+        // themed progress circular bars
+        if ( (j < 2) && !self.modes[j] && self.activated ) {
+          self.modes[j] = (j==0) ? 'buffer' : 'query';
+        }
+        if ( counter++ % 4 == 0 ) j++;
+        // Show the indicator in the "Used within Containers" after 200ms delay
+        if ( j == 2 ) self.contained = "indeterminate";
+    }, 100, 0, true);
+    $interval(function() {
+      self.mode = (self.mode == 'query' ? 'determinate' : 'query');
+    }, 7200, 0, true);
 
 
 
