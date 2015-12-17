eff0ff189cf07cdc2542ceb52af7f58c8ffff22d
Dylan Scheffers <dylan.scheffers@sablono.com>
2015-12-17 15:07:29 +0100

    dfs

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -65,7 +65,7 @@ $("#controlsSprint").submit(function() {
           SprintDataStoryDescription.push(response.feature.description.body.replace(/(<([^>]+)>)/ig,"").replace(/&nbsp;/gi,' ').replace(/&amp;/gi,' '));
           SprintDataStoryTitle.push(response.feature.name);
           storyPosition.push(response.feature.positions);
-          console.log(storyPosition);
+          console.log(response.feature.positions);
 
 
           // check if their is a epic
