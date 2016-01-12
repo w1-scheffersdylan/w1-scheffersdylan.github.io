7aa24b5a9ef9f78454cb247d6c5f67bae4af565d
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-12 12:01:05 +0100

    dfsdfs

--- js/createpdfStory.js
+++ js/createpdfStory.js
@@ -106,7 +106,8 @@ $("#controls").submit(function() {
 // EXTRA OPTIONS
       $("#btnDeleteNotes").click(function(){
         notesArraz.splice(0, notesArraz.length);
-        $("#btnDeleteNotes, #chechboxDeleteNotes").attr('ng-disabled', 'true');
+        $("#btnDeleteNotes, #chechboxDeleteNotes").hide();
+        $("#deleteNotesDone").show();
       });
 
             
