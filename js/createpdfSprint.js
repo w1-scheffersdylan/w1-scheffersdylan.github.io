20cf7d3e8fabd69b09e92e2f61e21de73e2de24d
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-13 11:53:52 +0100

    dsf

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -396,6 +396,10 @@ $("#controlsSprint").submit(function() {
 
       // create PDF (using PDFmake)
       $('#btnOpenPDFSprint').click(function () {
+
+        // EXTRA OPTIONS
+        var storyFontSize = parseFloat($("#sprintstoryFontSize").val());
+        var notesFontSize = parseFloat($("#sprintnotesFontSize").val());
           
        // remove ticket id from array of requirements
         for(var i = 0; i < SprintDataStoryRequirements.length; i++){
@@ -492,7 +496,7 @@ $("#controlsSprint").submit(function() {
                                  { text: 'REAL: \n' + '\n \n', colSpan: 2 }, '', '', ''],
 
                                 [{ text: [ 'STORY: \n \n', { text: SprintDataStoryDescription[storyPositionArray], style: 'mediumTextBold', alignment: 'center' }, '\n \n'],colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '', 
-                                 { text: ['NOTES:\n \n', { text: notesCheck == true ? SprintDataStoryNotes[storyPositionArray].join("\n") : "" }], colSpan: 6 }, '', '', '', '', ''],
+                                 { text: ['NOTES:\n \n', { text: notesCheck == true ? SprintDataStoryNotes[storyPositionArray].join("\n") : "", style: 'notesText' }], colSpan: 6 }, '', '', '', '', ''],
 
                                 [{ text: 'ACCEPTANCE CRITERIA: \n \n' + SprintDataStoryRequirements[storyPositionArray].join("\n"), colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '', 
                                  { text: 'DEFINITION OF DONE:\n \n __ Responsiveness \n \n __ Internationalization \n \n __ Code Review \n \n __ Documentation \n \n __ Testing by ....... \n \n __ Bug-fixing by ....... \n \n __ Linting & Beautify Code \n \n __ Quality assurance by PO \n \n __ Create pull request' , colSpan: 6 }, '', '', '', '', ''],
