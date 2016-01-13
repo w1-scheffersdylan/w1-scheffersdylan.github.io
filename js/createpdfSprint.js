32217b68b6f919d069d005997c3a5814d5bb01f4
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-13 11:23:18 +0100

    dfs

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -235,7 +235,7 @@ $("#controlsSprint").submit(function() {
         //SprintDataStoryNotes.splice(0, SprintDataStoryNotes.length, "");
         for(var i=0; i < SprintDataStoryNotes.length; i++){
           SprintDataStoryNotes[i] = "";
-          console.log("notes " + SprintDataStoryNotes);
+          console.log("notes " + SprintDataStoryNotes[0]);
         }
         $("#sprintbtnDeleteNotes, #sprintchechboxDeleteNotes").hide();
         $("#sprintdeleteNotesDone").show();
