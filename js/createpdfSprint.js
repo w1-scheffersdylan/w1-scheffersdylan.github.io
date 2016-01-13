ec6c7cee79d596a3eee2dec714a51cd304784d7c
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-13 11:24:44 +0100

    ljl

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -235,7 +235,7 @@ $("#controlsSprint").submit(function() {
         //SprintDataStoryNotes.splice(0, SprintDataStoryNotes.length, "");
         for(var i=0; i < SprintDataStoryNotes.length; i++){
           SprintDataStoryNotes[i] = "";
-          console.log("notes " + SprintDataStoryNotes[0]);
+          console.log("notes " + SprintDataStoryNotes[i]);
         }
         $("#sprintbtnDeleteNotes, #sprintchechboxDeleteNotes").hide();
         $("#sprintdeleteNotesDone").show();
