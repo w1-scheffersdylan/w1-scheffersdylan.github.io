56090add9ad6efca9c0a454ab18d84f7a111d0de
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-18 15:00:30 +0100

    ghfghf

--- js/createpdfStory.js
+++ js/createpdfStory.js
@@ -328,7 +328,7 @@ $("#controls").submit(function() {
                                 [{ text: [ 'STORY: \n \n', { text: storyDataDescription, style: 'mediumTextBold', alignment: 'center' }, '\n \n'],colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '', 
                                  { text: ['NOTES:\n \n', { text: notesArraz.join("\n"), style: 'notesText' }], colSpan: 6 }, '', '', '', '', ''],
 
-                                [{ text: 'STORY BRANCH: \n \n' ,colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '',
+                                [{ text: 'STORY BRANCH:' ,colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '',
                                 { text: 'DEFINITION OF DONE:\n \n __ Responsiveness \n \n __ Internationalization \n \n __ Code Review \n \n __ Documentation \n \n __ Testing by ....... \n \n __ Bug-fixing by ....... \n \n __ Linting & Beautify Code \n \n __ Quality assurance by PO \n \n __ Create pull request' , colSpan: 6, rowSpan: 2 }, '', '', '', '', ''],
 
                                 [{ text: 'ACCEPTANCE CRITERIA: \n \n' + requirementsArraz.join("\n"), colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', ''], 
