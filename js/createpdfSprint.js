311223c44caf911dc1d23d6b87e4b046f1493f98
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-19 11:30:02 +0100

    ds

--- js/createpdfSprint.js
+++ js/createpdfSprint.js
@@ -638,7 +638,7 @@ $("#controlsSprint").submit(function() {
                                  { text: ['NOTES:\n \n', { text: notesCheck == true ? SprintDataStoryNotes[storyPositionArray].join("\n") : "", style: 'notesText' }], colSpan: 6 }, '', '', '', '', ''],
 
                                 [{ text: 'STORY BRANCH: \n', margin: [0, 5, 0, 5], colSpan: 12 }, '', '', '', '', '', '', '', '', '', '', '',
-                                { text: 'TP: YES / NO', margin: [0, 5, 0, 5], colSpan: 2}, '',
+                                 { text: 'TP: YES / NO', margin: [0, 5, 0, 5], colSpan: 2}, '',
                                  { text: 'DEFINITION OF DONE:\n \n __ Responsiveness \n \n __ Internationalization \n \n __ Code Review \n \n __ Documentation \n \n __ Testing by ....... \n \n __ Bug-fixing by ....... \n \n __ Linting & Beautify Code \n \n __ Quality assurance by PO \n \n __ Create pull request' , colSpan: 6, rowSpan: 2 }, '', '', '', '', ''],
 
 
@@ -651,7 +651,8 @@ $("#controlsSprint").submit(function() {
 
                 }       
 
-            
+         
+                                
 
 
             // Save the PDF
