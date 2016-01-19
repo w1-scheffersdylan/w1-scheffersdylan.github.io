8716618053588dda7a3e36f3a1ea95f36c4638dc
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-19 11:20:47 +0100

    dfsdf

--- js/createpdfStory.js
+++ js/createpdfStory.js
@@ -331,7 +331,7 @@ $("#controls").submit(function() {
                                  { text: ['NOTES:\n \n', { text: notesArraz.join("\n"), style: 'notesText' }], colSpan: 6 }, '', '', '', '', ''],
 
                                 [{ text: 'STORY BRANCH: \n', margin: [0, 5, 0, 5], colSpan: 11 }, '', '', '', '', '', '', '', '', '', '', '',
-                                { text: 'TP: YES / NO', colSpan: 3}, '', '', '',
+                                { text: 'TP: YES / NO', colSpan: 3}, '', '',
                                 { text: 'DEFINITION OF DONE:\n \n __ Responsiveness \n \n __ Internationalization \n \n __ Code Review \n \n __ Documentation \n \n __ Testing by ....... \n \n __ Bug-fixing by ....... \n \n __ Linting & Beautify Code \n \n __ Quality assurance by PO \n \n __ Create pull request' , colSpan: 6, rowSpan: 2 }, '', '', '', '', ''],
 
                                 [{ text: 'ACCEPTANCE CRITERIA: \n \n' + requirementsArraz.join("\n"), colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', ''], 
