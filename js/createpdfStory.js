f4954172d39cb3972310ab37a10953561ed02e2b
Dylan Scheffers <dylan.scheffers@sablono.com>
2016-01-28 15:14:42 +0100

    adfs

--- js/createpdfStory.js
+++ js/createpdfStory.js
@@ -206,7 +206,7 @@ $("#controls").submit(function() {
                                 { text: 'DEFINITION OF DONE:\n \n __ Responsiveness \n \n __ Internationalization \n \n __ Code Review \n \n __ Documentation \n \n __ Testing by ....... \n \n __ Bug-fixing by ....... \n \n __ Linting & Beautify Code \n \n __ Quality assurance by PO \n \n __ Create pull request' , colSpan: 6, rowSpan: 3 }, '', '', '', '', ''],
 
                                 [{ text: 'ACCEPTANCE CRITERIA: \n \n' + requirementsArraz.join("\n"), colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '',
-                                { text: 'STORY BRANCH: \n', margin: [0, 5, 0, 5], colSpan: 14 }, '', '', '', '', '', '', '', '', '', '', '', '', '']
+                                { text: 'STORY BRANCH: \n', margin: [0, 5, 0, 5], colSpan: 6 }, '', '', '', '', '', '', '', '', '', '', '', '', '']
                             ]
                         }
                     }
